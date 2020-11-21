7c391b2a9264c012876607bf11f5e586c3a0c0f0
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-11-21 12:07:28 +0900

    add v-select html and css

--- app/javascript/view/Record.vue
+++ app/javascript/view/Record.vue
@@ -3,21 +3,26 @@
     <Title :title="title"></Title>
     <h3>{{ thisWeekDate() }}</h3>
     <div class="study-time-circle">
-      <h3 class="page-title">10h30m</h3>
+      <h3 class="page-title">{{ latestTotal }}h</h3>
     </div>
     <div><font-awesome-icon :icon="['fab', 'twitter-square']"/></div>
     <!-- todo formに変更 -->
     <div class="button-section">
-      <form>
-        <div class="custom-select">
-          <label for="select-choice1" class="label select-1"><span class="selection-choice">select study time</span></label>
-          <select v-model="time" id="time" class="select">
-            <option v-for="option in options" v-bind:value="option.value" :key="option.value">
-              {{ option.text }}
-            </option>
-          </select>
-        </div>
-        <button type="submit" class="study-time-button" @click="postStudyTime">register</button>
+      <div v-if="errors.length != 0">
+        <ul v-for=" e in errors" :key="e">
+          <li><font color="red">{{ e }}</font></li>
+        </ul>
+      </div>
+      <form @submit="postStudyTime">
+        <v-select v-model="study.time"
+          :options="options"
+          label="text"
+          :reduce="options => options.value"
+          placeholder="select study time"
+          class="style-chooser"
+          id="time">
+        </v-select>
+        <button type="submit" class="study-time-button">register</button>
       </form>
       <!-- todo <input type="hidden">totalをもたせる -->
     </div>
@@ -35,32 +40,44 @@ export default {
   },
   data() {
     return {
-      time:'0.5',
+      title: 'This Week Study Record',
       options: [
         { text: '0:30', value: '0.5'},
         { text: '1:00', value: '1'},
         { text: '1:30', value: '1.5'},
         { text: '2:00', value: '2'}
       ],
-      title: 'This Week Study Record',
+      latestTotal: '',
+      study: {
+        time: '',
+        total: '19'
+      },
+      errors: ''
     }
   },
+  mounted () {
+    // 最新の合計時間をjsonで取得
+    axios
+      .get(
+        'api/v1/studies.json'
+      )
+      .then(response => {
+        this.latestTotal = response.data
+      });
+  },
   methods: {
+    // 学習時間を登録する
     postStudyTime() {
-      axios.post(
-        'https://firestore.googleapis.com/v1/projects/everyday-study/databases/(default)/documents/times',
-        {
-          fields: {
-            time: {
-              doubleValue: this.time
-            }
-          }
-        }
-      ).then(response => {
-        console.log(response);
+      axios.post('/api/v1/studies', this.study)
+      .then(response => {
+        let e = response.data
+        this.$router.push('/');
       })
       .catch(error => {
         console.log(error);
+        if (error.response.data && error.response.data.erros) {
+          this.error = error.response.data.errors;
+        }
       });
     },
     // 今週の日付・曜日取得メソッド
@@ -70,14 +87,14 @@ export default {
       var this_month = today.getMonth(); //今月
       var date = today.getDate(); //今日の日付
       var day_num = today.getDay(); //今日の曜日 => 添字[0,1,2,3,4,5,6] 日~土
-      var this_monday = date - day_num + 1; //12 - 4 + 1 = 9(月)
-      var this_sunday = this_monday + 6; // 9 + 6 = 15(日)
+      var this_sunday = date - day_num; //12 - 4 = 8(日)
+      var this_saturday = this_sunday + 6; // 9 + 5 = 14(土)
       var day = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']; // day_numを文字列に変換
       // 月曜日 11/9(Mon)
-      var start_date = new Date(this_year, this_month, this_monday);
+      var start_date = new Date(this_year, this_month, this_sunday);
       start_date = (start_date.getMonth() + 1) + "/" + start_date.getDate() + " (" + day[start_date.getDay()] + ")"
       // 日曜日 11/15(Sun)
-      let end_date = new Date(this_year, this_month, this_sunday);
+      let end_date = new Date(this_year, this_month, this_saturday);
       end_date =  (end_date.getMonth() + 1) + "/" + end_date.getDate() + " (" + day[end_date.getDay()] +")"
       // 戻り値 11/9(Mon) ~ 11/15(Sun)
       var thisWeekDate = start_date + " ～ " + end_date;
@@ -120,43 +137,27 @@ h3.page-title {
   margin-top: 1em;
 }
 /* セレクトボックスレイアウト */
-.custom-select {
-  position : relative;
-  width: 100%;
-  margin: 2em auto;
-  cursor: pointer;
-}
-.select, .label {
-  display: block;
-}
-.select {
-  width: 100%;
-  position: absolute;
-  top: 0;
-  opacity: 0;
-}
-.selection-choice {
+.style-chooser .vs__search::placeholder, /*プレースホルダー*/
+.style-chooser .vs__dropdown-toggle, /*セレクトボックス全体*/
+.style-chooser .vs__dropdown-menu /*ドロップダウンメニュー*/{
+  background: #0066FF;
   color: white;
   font-weight: bold;
-  font-size: 1.8em;
+  font-size: 1.2em;
+  text-align: center;
+  border: none;
 }
-.label {
-  position: relative;
-  padding: 0.9em;
-  border-radius: .5em;
-  cursor: pointer;
+.style-chooser .vs__dropdown-menu {
+  background: white;
+  opacity: 0.9;
+  font-size: 1.6em;
+}
+.style-chooser .vs__clear /* × */,
+.style-chooser .vs__open-indicator/* ▽ */ {
+  fill: white;
+}
+.style-chooser .vs__selected /*選んだオプション*/ {
   color: white;
-}
-.label::after {
-  content: "▼";
-  position: absolute;
-  right: 0;
-  top: 0;
-  padding: 1.6em;
-  border-left: .1em solid white;
-}
-.select-1 {
-  background: #0066FF;
 }
 /* ここまで */
 </style>
