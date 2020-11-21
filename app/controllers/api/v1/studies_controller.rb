6caed95eab05531602e4f48d5fb9d6b98451379b
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-11-21 12:06:15 +0900

    add logic calc this week study time

--- app/controllers/api/v1/studies_controller.rb
+++ app/controllers/api/v1/studies_controller.rb
@@ -1,13 +1,25 @@
 class Api::V1::StudiesController < ApiController
   before_action :set_study, only: [:show]
 
-  rescue_from ActiveRecord::RecordNotFound do |exception|
-    render json: { error: '404 not found' }, status: 404
-  end
+  rescue_from Exception, with: :render_status_500
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_status_404
 
   def index
-    studies = Study.all
-    render json: studies
+    # todo currentUserロジック
+    # todo currentUserの今週日曜日~土曜日までのsum(:time)を取得する
+    latestTotal = Study.this_week_total
+    render json: latestTotal
+
+  end
+
+  def create
+    study = Study.new(study_params)
+    if study.save
+      render json: study, status: :created
+    else
+      render json: { errors: study.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -18,5 +30,17 @@ class Api::V1::StudiesController < ApiController
 
   def set_study
     @study = Study.find(params[:id])
+  end
+
+  def study_params
+    params.fetch(:study, {}).permit(:time, :total)
+  end
+
+  def render_status_404(exception)
+    render json: { errors: [exception] }, status: 404
+  end
+
+  def render_status_500(exception)
+    render json: { errors: [exception] }, status: 500
   end
 end
--- app/models/study.rb
+++ app/models/study.rb
@@ -1,4 +1,8 @@
 class Study < ApplicationRecord
   validates :time, presence: true, numericality: true
   validates :total, presence: true, numericality: true
+
+  def self.this_week_total
+    self.where(created_at: Date.today.beginning_of_week - 1..Date.today.end_of_week.end_of_day - 1.day).sum(:time)
+  end
 end
