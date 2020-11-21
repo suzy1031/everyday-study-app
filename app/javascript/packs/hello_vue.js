152eba2c01ab62e0e62af1e4a8d524b96261e451
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-11-21 12:06:45 +0900

    import vue-select

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -14,8 +14,12 @@ import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
+import vSelect from 'vue-select'
+import 'vue-select/dist/vue-select.css';
+
 library.add(fas, far, fab);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
+Vue.component('v-select', vSelect);
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
