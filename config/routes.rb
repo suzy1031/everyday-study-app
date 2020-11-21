1b5e0b06770265f17fb4a3a13e56a05f1d562bb5
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-11-21 12:06:31 +0900

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api, {format: 'json'} do
     namespace :v1 do
-      resources :studies, only: [:index, :show]
+      resources :studies, only: [:index, :create]
     end
   end
 end
