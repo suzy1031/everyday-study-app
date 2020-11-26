4627674ba09ee6cd118f880eae9af2829e06b1d2
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-11-27 08:27:04 +0900

    change valiable name

--- app/controllers/api/v1/studies_controller.rb
+++ app/controllers/api/v1/studies_controller.rb
@@ -8,8 +8,8 @@ class Api::V1::StudiesController < ApiController
   def index
     # todo currentUserロジック
     # todo currentUserの今週日曜日~土曜日までのsum(:time)を取得する
-    latestTotal = Study.this_week_total
-    render json: latestTotal
+    studies = Study.this_week_total
+    render json: studies
 
   end
 
