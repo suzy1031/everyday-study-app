adb6b49277bbf75c10d5f61487998ce766712d3e
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-12-01 10:15:06 +0900

    update get data logic

--- app/models/study.rb
+++ app/models/study.rb
@@ -4,9 +4,16 @@ class Study < ApplicationRecord
 
   def self.this_week_total
     @this_day = Date.today # 今日の日付
-    @this_sunday = @this_day.beginning_of_week(:sunday) # 日曜日
-    @this_saturday = @this_day.end_of_week(:sunday) # 土曜日
-    # 日曜日を週の始まりをする為、日〜土のデータ取得
-    self.where(created_at: @this_sunday..@this_saturday)
+    @this_date = @this_day.wday # 今日の曜日取得
+    range = @this_day.all_week(:sunday) # 日曜日始まりで今週の日〜土を取得
+    @data = Study.where(created_at: range) # 今週の日〜土のデータを取得
+
+    # 今週チェック
+    if (@this_date && @data.present?) # 今週の日〜月または今週のデータが1件以上の場合
+      self.where(created_at: range) # 今週の日〜土のデータ取得
+    else # 今週ではないかつ今週のデータが0件の場合
+      self.order(created_at: :desc).limit(1) # 最新レコードを1件取得
+    end
   end
+
 end
