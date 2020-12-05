d996eaa463e3a79fb9f1df9b2515052757d48652
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-12-05 09:51:55 +0900

    rspec model test

--- spec/models/study_spec.rb
+++ spec/models/study_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Study, type: :model do
-  describe "validate presence" do
-    context "timeを指定しているとき" do
-      it "レコードが作成される" do
+  describe 'Model Validation' do
+    context 'Success Create' do
+      it 'Valid params' do
         study = build(:study)
         study.valid?
         expect(study).to be_valid
+      end
+
+      it 'Check Float Type' do
+        study = build(:study)
+        expect(study.time).to be_a_kind_of(Float)
+        expect(study.total).to be_a_kind_of(Float)
       end
     end
   end
