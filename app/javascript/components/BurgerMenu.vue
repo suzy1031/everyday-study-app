34789ba337ea31fa9387ff5338b12b8d66743760
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-12-09 20:10:09 +0900

    create component

---
+++ app/javascript/components/BurgerMenu.vue
@@ -0,0 +1,19 @@
+<template>
+  <slide noOverlay>
+    <router-link to="/">
+      <span>Record</span>
+    </router-link>
+    <router-link to="/history">
+      <span>History</span>
+    </router-link>
+    <router-link to="/setting">
+      <span>Setting</span>
+    </router-link>
+  </slide>
+</template>
+<style>
+.bm-burger-button {
+  top: 3% !important;
+  left: 3% !important;
+}
+</style>
