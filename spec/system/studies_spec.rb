4548c288ac1b45c405b6ea31757ce93d6a5d96d8
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-12-10 08:30:33 +0900

    fix system test

--- spec/system/studies_spec.rb
+++ spec/system/studies_spec.rb
@@ -9,8 +9,7 @@ RSpec.describe 'Record.vue feature test', type: :system do
     expect(page).to have_css '.page-title' # 学習時間
     expect(page).to have_css '.page-sub-title' # 合計時間
     expect(page).to have_no_button 'register' # 登録ボタン非表示
-    page.find('div.v-select').click # セレクトボックスをクリック
-    page.find('ul.vs__dropdown-menu', text: '0:30').click # 学習時間を選択
+    select '0:30', from: 'time' # 学習時間を選択
     expect(page).to have_button 'register' # 登録ボタン表示
     click_button 'register' # 登録ボタン押下
   end
