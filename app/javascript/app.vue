0a1ce7bb7ec0531d0c538aee8327abe9a9b1296a
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-12-12 12:09:51 +0900

    delete router config

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -5,46 +5,9 @@
 </template>
 
 <script>
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Record from './view/Record';
-import History from './view/History';
-import Setting from './view/Setting';
-import Login from './view/Login';
-import Signup from './view/Signup';
-import Store from './store/store.js';
-
-const router = new VueRouter({
-  mode: 'history',
-  routes: [
-    {
-      path: '/',
-      component: Record
-    },
-    {
-      path: '/history',
-      component: History
-    },
-    {
-      path: '/setting',
-      component: Setting
-    },
-    {
-      path: '/login',
-      component: Login
-    },
-    {
-      path: '/signup',
-      component: Signup
-    }
-  ]
-})
-
-Vue.use(VueRouter)
 
 export default {
-  router,
-  store: Store,
+  name: 'App',
   data() {
     return {
     }
