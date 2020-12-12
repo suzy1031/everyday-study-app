575dcfa85f0a31458445155dfaca8d2879e6712f
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-12-12 11:47:25 +0900

    add validation in user model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  before_save { email.downcase! }
+  validates :email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+end
