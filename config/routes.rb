bc19c790e08b434d4bed8be9a0e795652a9e417c
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-12-12 11:50:45 +0900

    add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   namespace :api, {format: 'json'} do
-    namespace :v1 do
-      resources :studies, only: [:index, :create]
-    end
+    resources :studies, only: [:index, :create]
+    post   'signup',  controller: :users,    action: :create
+    post   'signin',  controller: :sessions, action: :create
+    delete 'signin', controller: :sessions, action: :destroy
+    post   'refresh', controller: :refresh,  action: :create
   end
 end
