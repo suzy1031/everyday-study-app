3f0d83039342435aabbc5194130b5e06924acacd
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-12-16 08:30:26 +0900

    build auth function

--- app/javascript/view/Login.vue
+++ app/javascript/view/Login.vue
@@ -2,14 +2,17 @@
   <div>
     <Title :title="title"></Title>
     <h4>Please Login And enjoy App!!</h4>
-    <!-- todo formに変更 -->
-    <div class="login-section">
-      <label for="email" class="form-label">Email</label>
-      <input id="email" type="email">
-      <label for="password" class="form-label">Password</label>
-      <input id="password" type="password">
-      <router-link to="/signup" class="signup-path">Sign Up</router-link>
-    </div>
+    <form @submit.prevent="signin">
+      <div class="common-error-message" v-if="error">{{ error }}</div>
+      <div class="login-section">
+        <label for="email" class="form-label">Email</label>
+        <input v-model="email" id="email" type="email" required="required" autocomplete="off">
+        <label for="password" class="form-label">Password</label>
+        <input v-model="password" id="password" type="password" required="required" autocomplete="off">
+        <router-link to="/signup" class="signup-path">Sign Up</router-link>
+      </div>
+      <button type="submit" class="study-time-button">Sign In</button>
+    </form>
   </div>
 </template>
 
@@ -17,37 +20,73 @@
 import Title from '../components/Title';
 
 export default {
+  name: 'Signin',
   components: {
     Title
   },
   data() {
     return {
-      title: "Login"
+      title: "Login",
+      email: '',
+      password: '',
+      error: ''
+    }
+  },
+  created() {
+    this.checkSignedIn()
+  },
+  updated() {
+    this.checkSignedIn()
+  },
+  methods: {
+    signin() {
+      this.$http.plain.post('/api/signin', { email: this.email, password: this.password })
+      .then(response => this.signinSuccessful(response))
+      .catch(error => this.signinFailed(error))
+    },
+    signinSuccessful(response) {
+      if (!response.data.csrf) {
+        this.signinFailed(response)
+        return
+      }
+      localStorage.csrf = response.data.csrf
+      localStorage.signedIn = true
+      this.$store.dispatch('doFetchSignedIn')
+      this.error = ''
+      this.$router.replace('/Record')
+    },
+    signinFailed(error) {
+      this.error = (error.response && error.response.data && error.response.data.error) || ''
+      delete localStorage.csrf
+      delete localStorage.signedIn
+    },
+    checkSignedIn() {
+      if (localStorage.signedIn) {
+        this.$router.replace('/')
+      }
     }
   }
 }
 </script>
 <style>
 .login-section {
-  position: absolute;
-  width: 300px;
-  height: 250px;
+  width: 30em;
+  height: 22em;
   background: #0066FF;
-  border-radius: 4px;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
+  border-radius: .3em;
   display:flex;
   flex-flow: column;
+  margin: 0 auto;
+  margin-top: 5em;
 }
 .form-label {
   color: white;
-  font-size: 24px;
+  font-size: 2.4em;
   font-weight: bold;
-  margin: 5px 0;
+  margin: .2em 0;
 }
 .signup-path {
-  margin: 10px;
+  margin: .4em;
   font-weight: bold;
   text-decoration: underline;
 }
