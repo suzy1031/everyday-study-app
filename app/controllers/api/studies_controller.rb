6ca51fd423ae3b49c662fc3db24de04da0c594b4
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-12-16 08:29:05 +0900

    add current_user logic

--- app/controllers/api/studies_controller.rb
+++ app/controllers/api/studies_controller.rb
@@ -1,41 +1,29 @@
 class Api::StudiesController < ApplicationController
   protect_from_forgery except: [:index, :create, :update, :destroy]
 
-  before_action :set_study, only: [:show]
+  before_action :authorize_access_request!
 
   rescue_from Exception, with: :render_status_500
-
   rescue_from ActiveRecord::RecordNotFound, with: :render_status_404
 
   def index
-    # todo currentUserロジック
-    # todo currentUserの今週日曜日~土曜日までのデータを取得する
-    studies = Study.this_week_total
-    # studies = Study.all
+    studies = current_user.studies.this_week_total
     render json: studies
   end
 
   def create
-    study = Study.new(study_params)
+    study = current_user.studies.build(study_params)
     if study.save
       render json: study, status: :created
     else
-      render json: { errors: study.errors.full_messages }, status: :unprocessable_entity
+      render json: study.errors, status: :unprocessable_entity
     end
-  end
-
-  def show
-    render json: @study
   end
 
   private
 
-  def set_study
-    @study = Study.find(params[:id])
-  end
-
   def study_params
-    params.fetch(:study, {}).permit(:time, :total)
+    params.require(:study).permit(:time, :total, :user_id)
   end
 
   def render_status_404(exception)
