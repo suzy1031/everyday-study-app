9b6c9b313bd52853c5313615ad5295610900226c
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-12-19 00:08:51 +0900

    build users rspec

--- spec/requests/api/users_request_spec.rb
+++ spec/requests/api/users_request_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "Api::Users", type: :request do
+RSpec.describe 'Api::UsersController' do
+  describe 'POST #create' do
+    let(:user_params) { { email: 'test@email.com', password: 'password', password_confirmation: 'password' } }
 
+    context 'creates a new user' do
+      before do
+        expect{ post '/api/signup', { params: user_params } }.to change(User, :count).by(1)
+      end
+
+      it 'returns http success' do
+        expect(response).to be_successful
+        expect(response_json.keys).to eq ['csrf']
+        expect(response.cookies[JWTSessions.access_cookie]).to be_present
+      end
+    end
+  end
 end
