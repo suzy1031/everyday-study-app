74284acd59ae4a65cbe394b7e46dbbad156750b6
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-12-20 11:11:37 +0900

    build rspec test users and sessions

--- spec/requests/api/sessions_request_spec.rb
+++ spec/requests/api/sessions_request_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe "Api::Sessions", type: :request do
+RSpec.describe "Api::SessionsController" do
+  let(:user) { create(:user) }
 
+  describe 'POST #create' do
+    let(:password) { 'password' }
+    let(:user_params) { { email: user.email, password: password } }
+
+    it 'Returns http success' do
+      post '/api/signin', params: user_params
+      expect(response).to be_successful
+      expect(response_json.keys).to eq ['csrf']
+      expect(response.cookies[JWTSessions.access_cookie]).to be_present
+    end
+
+    it 'Returns unauthorized for invalid params' do
+      post '/api/signin', params: { email: user.email, password: 'incorrect' }
+      expect(response).to have_http_status(404)
+    end
+  end
 end
--- spec/requests/api/users_request_spec.rb
+++ spec/requests/api/users_request_spec.rb
@@ -4,15 +4,20 @@ RSpec.describe 'Api::UsersController' do
   describe 'POST #create' do
     let(:user_params) { { email: 'test@email.com', password: 'password', password_confirmation: 'password' } }
 
-    context 'creates a new user' do
+    context 'Creates a new user' do
       before do
         expect{ post '/api/signup', { params: user_params } }.to change(User, :count).by(1)
       end
 
-      it 'returns http success' do
+      it 'Returns http success' do
         expect(response).to be_successful
         expect(response_json.keys).to eq ['csrf']
         expect(response.cookies[JWTSessions.access_cookie]).to be_present
+      end
+
+      it 'Returns unauthorized for invalid params' do
+        post '/api/signup', params: { email: 'test@mail.com', password: 'password', password_confirmation: 'incorrect' }
+        expect(response).to have_http_status(422)
       end
     end
   end
