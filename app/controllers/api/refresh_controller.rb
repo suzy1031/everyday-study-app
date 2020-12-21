54423214d866422423f99f2ef31bb96667b20070
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-12-21 10:40:11 +0900

    change type request => controller

--- app/controllers/api/refresh_controller.rb
+++ app/controllers/api/refresh_controller.rb
@@ -2,7 +2,8 @@ class Api::RefreshController < ApplicationController
   before_action :authorize_refresh_by_access_request!
 
   def create
-    session = JWTSessions::Session.new(payload: claimless_payload, refresh_by_access_allowed: true)
+    session = JWTSessions::Session.new(payload: claimless_payload,
+                                      refresh_by_access_allowed: true)
     tokens = session.refresh_by_access_payload do
       raise JWTSessions::Errors::Unauthorized, "Somethings not right here!"
     end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
-
-RSpec.describe "Api::SessionsController" do
+RSpec.describe Api::SessionsController, type: :controller do
   let(:user) { create(:user) }
 
   describe 'POST #create' do
     let(:password) { 'password' }
     let(:user_params) { { email: user.email, password: password } }
+    let(:user_invalid_params) { { email: user.email, password: 'incorrect' } }
 
-    it 'Returns http success' do
-      post '/api/signin', params: user_params
+    it 'returns http success' do
+      post :create, params: user_params
       expect(response).to be_successful
       expect(response_json.keys).to eq ['csrf']
       expect(response.cookies[JWTSessions.access_cookie]).to be_present
     end
 
-    it 'Returns unauthorized for invalid params' do
-      post '/api/signin', params: { email: user.email, password: 'incorrect' }
+    it 'returns unauthorized for invalid params' do
+      post :create, params: user_invalid_params
       expect(response).to have_http_status(404)
     end
   end
