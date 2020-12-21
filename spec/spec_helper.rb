4e69237884b2b441c072b6e6618f1047dfe6fdbd
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-12-21 10:40:26 +0900

    build helper setting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,7 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require_relative 'support/auth_helper'
 require_relative 'support/response_helper'
 RSpec.configure do |config|
   config.include ResponseHelper
@@ -39,6 +40,8 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+  config.include ResponseHelper
+  config.include AuthHelper
   # This option will default to `:apply_to_host_groups` in RSpec 4 (and will
   # have no way to turn it off -- the option exists only for backwards
   # compatibility in RSpec 3). It causes shared context metadata to be
