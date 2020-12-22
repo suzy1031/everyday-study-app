1858408a5f3945292ebcd8d991203876f50e5551
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-12-23 08:28:14 +0900

    test: user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    context 'success' do
+      before do
+        @user = FactoryBot.create(:user)
+      end
+
+      it 'is valid with a email, password, password_confirmation' do
+        user = @user
+        expect(user).to be_valid
+      end
+
+      it 'is null with a email' do
+        expect(FactoryBot.build(:user, email: '')).to_not be_valid
+      end
+
+      it 'is duplicate with a email' do
+        anoter_user = FactoryBot.create(:user, email: 'test@email.com')
+        expect(FactoryBot.build(:user, email: 'test@email.com')).to_not be_valid
+      end
+
+      it 'is not match pattern with a password' do
+        expect(FactoryBot.build(:user, email: 'testemail.com')).to_not be_valid
+      end
+
+      it 'is null with a password' do
+        expect(FactoryBot.build(:user, password: '')).to_not be_valid
+      end
+
+      it 'is shorter than 6 charactors with a password' do
+        expect(FactoryBot.build(:user, password: 'test')).to_not be_valid
+      end
+
+      it 'is longer than 20 charactors with a password' do
+        expect(FactoryBot.build(:user, password: 'thisislongerthan20charactorspassword')).to_not be_valid
+      end
+
+      it 'is encryption with a password' do
+        user = @user
+        expect(user.password_digest).to_not eq 'password'
+      end
+
+      it 'is not match with a password and password_confirmation' do
+        user = @user
+        expect(FactoryBot.build(:user, password: 'password', password_confirmation: 'incorrect')).to_not be_valid
+      end
+    end
+  end
 end
