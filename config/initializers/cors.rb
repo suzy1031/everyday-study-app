31a0adca3a50ddf99c17ab16c9bb34a90401e67c
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-12-27 08:55:53 +0900

    conf: add URL cors setting

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8080', 'http://localhost:3000', 'http://localhost:5000'
+    origins 'http://localhost:8080', 'http://localhost:3000', 'http://localhost:5000', 'https://still-dusk-87527.herokuapp.com/'
 
     resource '*',
       headers: :any,
