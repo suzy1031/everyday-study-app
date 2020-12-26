b0c03ef4facdec2803107eeb4c99585ba5f03080
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-12-26 11:32:17 +0900

    conf: fixAdd2 redis config

--- config/initializers/session_store.rb
+++
@@ -1,9 +0,0 @@
-# config/initializers/session_store.rb
-if Rails.env.production?
-
-  # Production では session_store に Redis を利用する
-  Rails.application.config.session_store :redis_store, {
-    servers: ENV['REDIS_URL'],
-    expire_after: 1.week
-  }
-end
