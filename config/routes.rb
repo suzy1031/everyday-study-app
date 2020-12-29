33bdea34f77217277c56ee7d6b8cc0f4c55b6455
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-12-29 11:36:48 +0900

    src: add goal routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api, {format: 'json'} do
     namespace :v1 do
       resources :studies, only: [:index, :create]
+      resources :goals, only: [:index, :create, :update]
     end
     post   'signup',  controller: :users,    action: :create
     post   'signin',  controller: :sessions, action: :create
