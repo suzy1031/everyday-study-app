a090ea52e03c08e6155cfa34fef4d75286e0eaba
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-12-31 11:41:34 +0900

    src: change column type

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_28_022953) do
+ActiveRecord::Schema.define(version: 2020_12_31_011151) do
 
   create_table "goals", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "target_time", null: false
+    t.float "target_time", null: false
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
