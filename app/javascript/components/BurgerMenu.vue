47923f843a61b61015d8a88188188a3da3825c98
suzy1031 <kohei.sugawara.1031@gmail.com>
2021-01-02 17:07:41 +0900

    src: add log out path

--- app/javascript/components/BurgerMenu.vue
+++ app/javascript/components/BurgerMenu.vue
@@ -9,8 +9,35 @@
     <router-link to="/setting">
       <span>Setting</span>
     </router-link>
+    <a href="/" v-if="signedIn" @click="signOut">
+      <span>Logout</span>
+    </a>
   </slide>
 </template>
+<script>
+import { mapState } from 'vuex'
+export default {
+  computed: mapState([
+    'signedIn'
+  ]),
+  mounted () {
+    this.$store.dispatch('doFetchSignedIn')
+  },
+  methods: {
+    setError(error, text) {
+      this.error = (error.response && error.response.data && error.response.data.error) || text
+    },
+    signOut() {
+      this.$http.secured.delete(`/api/signin`)
+      .then(response => {
+        delete localStorage.csrf
+        delete localStorage.signedIn
+      })
+      .catch(error => this.setError(error, 'Cannot sign out'))
+    }
+  }
+}
+</script>
 <style>
 .bm-burger-button {
   top: 3% !important;
