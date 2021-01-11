5ec2a9f9788f58f750150a8a11ea039d6e3cb611
suzy1031 <kohei.sugawara.1031@gmail.com>
2021-01-11 12:23:08 +0900

    spec: add create data spec

--- spec/models/study_spec.rb
+++ spec/models/study_spec.rb
@@ -2,9 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Study, type: :model do
   describe 'model validation' do
+    let(:study) { FactoryBot.create(:study) }
+
     context 'success create' do
-      it 'valid params' do
-        study = build(:study)
+      it 'create data' do
+        user = User.create(email: 'test@test.com', password: 'password')
+        user.studies.create(time: '0.5', total: '1')
+        expect{ study }.to change{ Study.count }.by(1)
+      end
+
+      it 'is with a valid params' do
         study.valid?
         expect(study).to be_valid
       end
@@ -13,6 +20,20 @@ RSpec.describe Study, type: :model do
         study = build(:study)
         expect(study.time).to be_a_kind_of(Float)
         expect(study.total).to be_a_kind_of(Float)
+      end
+    end
+
+    context 'failure create' do
+      it 'is null with a time' do
+        expect(FactoryBot.build(:study, time: nil, total: '1')).to_not be_valid
+      end
+
+      it 'is null with a total' do
+        expect(FactoryBot.build(:study, time: '0.5', total: nil)).to_not be_valid
+      end
+
+      it 'is more than 24 hour with a time' do
+        expect(FactoryBot.build(:study, time: '25', total: '25')).to_not be_valid
       end
     end
   end
