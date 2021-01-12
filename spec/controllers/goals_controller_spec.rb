021f1ad4b98c602a3098db51aa3e50c041d3089f
suzy1031 <kohei.sugawara.1031@gmail.com>
2021-01-12 18:04:06 +0900

    src: add update spec

--- spec/controllers/goals_controller_spec.rb
+++ spec/controllers/goals_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Api::V1::GoalsController, type: :controller do
   let(:user) { create(:user) }
 
   let(:valid_params) {
-    { target_time: 10 }
+    { target_time: 10.0 }
   }
 
   let(:invalid_params) {
@@ -38,7 +38,7 @@ RSpec.describe Api::V1::GoalsController, type: :controller do
   describe 'POST #create' do
     context 'with a valid params' do
       it 'create a new goal' do
-        request.cookie[JWTSessions.access_cookie] = @tokens[:access]
+        request.cookies[JWTSessions.access_cookie] = @tokens[:access]
         request.headers[JWTSessions.csrf_header] = @tokens[:csrf]
         expect {
           post :create, params: { goal: valid_params }
@@ -46,7 +46,7 @@ RSpec.describe Api::V1::GoalsController, type: :controller do
       end
 
       it 'returns a JSON response with the new goal' do
-        request.cookie[JWTSessions.access_cookie] = @tokens[:access]
+        request.cookies[JWTSessions.access_cookie] = @tokens[:access]
         request.headers[JWTSessions.csrf_header] = @tokens[:csrf]
         post :create, params: { goal: valid_params }
         expect(response).to have_http_status(:created)
@@ -54,7 +54,7 @@ RSpec.describe Api::V1::GoalsController, type: :controller do
       end
 
       it 'is unauth without CSRF' do
-        request.cookie[JWTSessions.access_cookie] = @tokens[:access]
+        request.cookies[JWTSessions.access_cookie] = @tokens[:access]
         post :create, params: { goal: valid_params }
         expect(response).to have_http_status(500)
       end
@@ -62,11 +62,46 @@ RSpec.describe Api::V1::GoalsController, type: :controller do
 
     context 'with a invalid params' do
       it 'renders a JSON response with errors for the new goal' do
-        request.cookie[JWTSessions.access_cookie] = @tokens[:access]
+        request.cookies[JWTSessions.access_cookie] = @tokens[:access]
         request.headers[JWTSessions.csrf_header] = @tokens[:csrf]
         post :create, params: { goal: invalid_params }
         expect(response).to have_http_status(:unprocessable_entity)
         expect(response.content_type).to eq('application/json')
+      end
+    end
+
+    describe 'PUT #update' do
+      let!(:goal) { create(:goal, user: user) }
+
+      context 'with a valid params' do
+        let(:new_params) {
+          { target_time: 12.0 }
+        }
+        it 'updates the requested goal' do
+          request.cookies[JWTSessions.access_cookie] = @tokens[:access]
+          request.headers[JWTSessions.csrf_header] = @tokens[:csrf]
+          put :update, params: { id: goal.id, goal: new_params }
+          goal.reload
+          expect(goal.target_time).to eq new_params[:target_time]
+        end
+
+        it 'renders a JSON response with the todo' do
+          request.cookies[JWTSessions.access_cookie] = @tokens[:access]
+          request.headers[JWTSessions.csrf_header] = @tokens[:csrf]
+          put :update, params: { id: goal.to_param, goal: valid_params }
+          expect(response).to have_http_status(:ok)
+          expect(response.content_type).to eq('application/json')
+        end
+      end
+
+      context 'with invalid params' do
+        it 'renders a JSON with errors for the goal' do
+          request.cookies[JWTSessions.access_cookie] = @tokens[:access]
+          request.headers[JWTSessions.csrf_header] = @tokens[:csrf]
+          put :update, params: { id: goal.to_param, goal: invalid_params }
+          expect(response).to have_http_status(500)
+          expect(response.content_type).to eq('application/json')
+        end
       end
     end
   end
