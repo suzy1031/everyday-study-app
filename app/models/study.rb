bb788d382e644df1930a271768ac1e7d19078956
suzy1031 <kohei.sugawara.1031@gmail.com>
2021-01-28 07:57:40 +0900

    src: add get chart data from study model

--- app/models/study.rb
+++ app/models/study.rb
@@ -6,6 +6,7 @@ class Study < ApplicationRecord
             numericality: { less_than_or_equal_to: 24 }
   validates :total, presence: true, numericality: true
 
+  # studies_controller #index
   def self.this_week_total
     @this_day = Time.now # 今日の日付
     @this_date = @this_day.wday # 今日の曜日取得
@@ -18,6 +19,18 @@ class Study < ApplicationRecord
     else # 今週ではないかつ今週のデータが0件の場合
       self.order(created_at: :desc).limit(1) # 最新レコードを1件取得
     end
+  end
+
+  # studies_controller #histories
+  def self.get_week_chart_data
+    @this_day = Time.now
+    @range = @this_day.all_week(:sunday)
+    # 今週の日曜日~土曜日までの範囲
+    # id, time日別合計値, user_id, created_at, 曜日(添字)を取得
+    # 日別データ毎にグループ化する
+    self.where(created_at: @range)
+        .select(:id, "sum(time) as time", :user_id, :created_at, "dayofweek(created_at) - 1 as day_of_week")
+        .group("date(created_at)")
   end
 
 end
